89fb976a1a4312d2e72eb4b638135047fd4bc3c3
stepanver <stepan.ver11@gmail.com>
2018-11-26 21:04:51 +0900

    add layout

---
+++ layouts/single.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <span>signle layout</span>
+        <AppNavigation />
+        <hr>
+
+        <nuxt />
+        <nuxt-link to="/">トップへ戻る</nuxt-link>
+        <hr>
+        
+        <footer>
+            single footer
+        </footer>
+    </div>
+</template>
+
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+
+export default {
+    components: {
+        AppNavigation
+    }
+}
+</script>
+
